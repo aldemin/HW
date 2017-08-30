d66b63197533422f3a6ac2a990a6dd679ae509ab
Alexandr Demin <cargiman@mail.ru>
2017-08-30 22:02:07 +0300

    init

---
+++ 5 lesson/src/com/geekbrains/MainClass.java
@@ -0,0 +1,24 @@
+package com.geekbrains;
+
+import java.util.concurrent.CyclicBarrier;
+
+public class MainClass {
+    public static final int CARS_COUNT = 4;
+    private static final CyclicBarrier cb = new CyclicBarrier(CARS_COUNT);
+
+    public static void main(String[] args) {
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+        Race race = new Race(new Road(60), new Tunnel(CARS_COUNT), new Road(40));
+        Car[] cars = new Car[CARS_COUNT];
+        for (int i = 0; i < cars.length; i++) {
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10), cb);
+        }
+        for (int i = 0; i < cars.length; i++) {
+            new Thread(cars[i]).start();
+        }
+
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+    }
+}
+
