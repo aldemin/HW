383910cbdbabebc2067ab412418a5d8bd21eebbf
Alexandr Demin <cargiman@mail.ru>
2017-08-31 17:13:13 +0300

    правильный вывод важных сообщений

--- 5 lesson/src/com/geekbrains/Car.java
+++ 5 lesson/src/com/geekbrains/Car.java
@@ -6,7 +6,8 @@ import java.util.concurrent.atomic.AtomicBoolean;
 public class Car implements Runnable {
     private static AtomicBoolean isWin = new AtomicBoolean(true);
     private static int CARS_COUNT;
-    private final CyclicBarrier cb;
+    private final CyclicBarrier cbStartRace;
+    private final CyclicBarrier cbFinishRace;
     static {
         CARS_COUNT = 0;
     }
@@ -20,12 +21,13 @@ public class Car implements Runnable {
         return speed;
     }
 
-    public Car(Race race, int speed, CyclicBarrier cb) {
+    public Car(Race race, int speed, CyclicBarrier cbStartRace, CyclicBarrier cbFinishRace) {
         this.race = race;
         this.speed = speed;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
-        this.cb = cb;
+        this.cbStartRace = cbStartRace;
+        this.cbFinishRace = cbFinishRace;
     }
     @Override
     public void run() {
@@ -33,17 +35,19 @@ public class Car implements Runnable {
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int)(Math.random() * 800));
             System.out.println(this.name + " готов");
-            cb.await();
+            cbStartRace.await();
+
+            for (int i = 0; i < race.getStages().size(); i++) {
+                race.getStages().get(i).go(this);
+            }
+
+            if (isWin.get()) {
+                System.out.println(this.name + "!!!!!!WIN!!!!!");
+                isWin.set(false);
+            }
+            cbFinishRace.await();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        for (int i = 0; i < race.getStages().size(); i++) {
-            race.getStages().get(i).go(this);
-        }
-        if (isWin.get()) {
-            System.out.println(this.name + "!!!!!!WIN!!!!!");
-            isWin.set(false);
-        }
-
     }
 }
--- 5 lesson/src/com/geekbrains/MainClass.java
+++ 5 lesson/src/com/geekbrains/MainClass.java
@@ -4,21 +4,23 @@ import java.util.concurrent.CyclicBarrier;
 
 public class MainClass {
     public static final int CARS_COUNT = 4;
-    private static final CyclicBarrier cb = new CyclicBarrier(CARS_COUNT);
+    private static final CyclicBarrier cbStartRace = new CyclicBarrier(CARS_COUNT, ()->{
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+    });
+    private static final CyclicBarrier cbFinishRace = new CyclicBarrier(CARS_COUNT, ()->{
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+    });
 
     public static void main(String[] args) {
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
         Race race = new Race(new Road(60), new Tunnel(CARS_COUNT), new Road(40));
         Car[] cars = new Car[CARS_COUNT];
         for (int i = 0; i < cars.length; i++) {
-            cars[i] = new Car(race, 20 + (int) (Math.random() * 10), cb);
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10), cbStartRace, cbFinishRace);
         }
         for (int i = 0; i < cars.length; i++) {
             new Thread(cars[i]).start();
         }
-
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
     }
 }
 
