56d3b75cc07b5bc32fc8216dfa445aaabb63afb6
Alexandr Demin <cargiman@mail.ru>
2017-09-03 21:20:00 +0300

    поправил логику

--- 6 lesson/src/main/java/ru/geekbrains/OneOrFour.java
+++ 6 lesson/src/main/java/ru/geekbrains/OneOrFour.java
@@ -1,18 +1,27 @@
 package ru.geekbrains;
 
-import java.util.Arrays;
-
 public class OneOrFour {
 
     public boolean oneOrFour(int[] arr) {
         final int sizeArr = arr.length;
+        boolean oneExist = false;
+        boolean fourExist = false;
 
         for (int i = 0; i < sizeArr; i++) {
             if (arr[i] != 1 && arr[i] != 4) {
                 return false;
             }
+            if (arr[i] == 1) {
+                oneExist = true;
+            } else {
+                fourExist = true;
+            }
         }
-        Arrays.sort(arr);
-        return arr[0] != arr[sizeArr - 1];
+        if (oneExist && fourExist) {
+            return true;
+        } else {
+            return false;
+        }
+
     }
 }
